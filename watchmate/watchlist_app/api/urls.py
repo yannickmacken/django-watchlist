9c8cf0221a4ffea280c42b20baa68fa4bc8f46d9
yannickmacken <yannickmacken@gmail.com>
2022-05-08 21:53:34 +0200

    create first rest api methods

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from watchlist_app.views import movie_list
-from watchlist_app.views import movie_details
+from watchlist_app.api.views import movie_list
+from watchlist_app.api.views import movie_details
 
 urlpatterns = [
     path('list/', movie_list, name='movie_list'),
---
+++ watchmate/watchlist_app/api/views.py
@@ -0,0 +1,29 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from watchlist_app.models import Movie
+from watchlist_app.api.serializers import MovieSerializer
+
+@api_view(['GET', 'POST'])
+def movie_list(request):
+    
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+            
+@api_view(['GET', 'PUT', 'DELETE'])
+def movie_details(request, movie_id):
+    
+    if request.method == 'GET':
+        movie = Movie.objects.get(pk=movie_id)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+    
